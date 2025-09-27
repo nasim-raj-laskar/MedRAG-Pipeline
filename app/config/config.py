fe8abf17fcb607465046cad745088db39d8d5e2e
NASIM <nosytub@gmail.com>
2025-09-27 17:36:11 +0530

    Configuration Initialized

---
+++ app/config/config.py
@@ -0,0 +1,8 @@
+import os
+HF_TOKEN=os.environ.get("HF_TOKEN")
+
+HUGGINGFACE_REPO_ID="openai/gpt-oss-120b"
+DB_FAISS_PATH="vectorstore/db_faiss"
+DATA_PATH="data/"
+CHUNK_SIZE=500
+CHUNK_OVERLAP=50
